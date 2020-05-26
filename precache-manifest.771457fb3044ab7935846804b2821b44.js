a74bf3c420ea71e985bfc9d6e13f6583fad6592a
Raphael Zueblin <raphaelzublin@yahoo.com.br>
2020-05-26 15:15:37 +0200

    Updates

---
+++ precache-manifest.771457fb3044ab7935846804b2821b44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a0430fa9413ec13af3e429b60b51086",
+    "url": "/github-finder/index.html"
+  },
+  {
+    "revision": "9ac21701d15e92c1d4cf",
+    "url": "/github-finder/static/css/main.49d11ba2.chunk.css"
+  },
+  {
+    "revision": "11fbcf09980cabbb6368",
+    "url": "/github-finder/static/js/2.6c1d6624.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-finder/static/js/2.6c1d6624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ac21701d15e92c1d4cf",
+    "url": "/github-finder/static/js/main.c725e8b7.chunk.js"
+  },
+  {
+    "revision": "e2c8255bce1eb204bf1a",
+    "url": "/github-finder/static/js/runtime-main.bcd3cdab.js"
+  }
+]);
