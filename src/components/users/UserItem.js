340559063078caa273b6d28eb2499a2e47aea996
Raphael Zueblin <raphaelzublin@yahoo.com.br>
2020-05-27 06:42:21 +0200

    add img link

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -5,12 +5,14 @@ import { Link } from "react-router-dom";
 const UserItem = ({ user: { login, avatar_url, html_url } }) => {
   return (
     <div className="card text-center">
-      <img
-        src={avatar_url}
-        alt={login}
-        className="round-img"
-        style={{ width: "60px" }}
-      />
+      <Link to={`/user/${login}`}>
+        <img
+          src={avatar_url}
+          alt={login}
+          className="round-img"
+          style={{ width: "60px" }}
+        />
+      </Link>
       <h3>{login}</h3>
       <div>
         <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
