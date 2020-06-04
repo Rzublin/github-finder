dcf9f95b8401a01a15398ca1b152c27de3c1e1a8
Raphael Zueblin <raphaelzublin@yahoo.com.br>
2020-06-04 20:52:01 +0200

    add context reducer

---
+++ src/context/github/githubReducer.js
@@ -0,0 +1,16 @@
+import * as githubTypes from "../types";
+
+function githubReducer(state, action) {
+  switch (action.type) {
+    case githubTypes.SEARCH_USERS:
+      return { ...state, users: action.payload };
+    case githubTypes.CLEAR_USERS:
+      return;
+    case githubTypes.GET_USER:
+      return { ...state, user: action.payload };
+    default:
+      return state;
+  }
+}
+
+export default githubReducer;
